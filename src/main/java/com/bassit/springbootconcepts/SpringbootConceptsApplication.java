f65ac14c02be6f349a1a61ca0e091edf986ae209
Bassit <bassitilahi19@gmail.com>
2024-01-30 10:01:16 -0500

    Initial Commit

---
+++ src/main/java/com/bassit/springbootconcepts/SpringbootConceptsApplication.java
@@ -0,0 +1,13 @@
+package com.bassit.springbootconcepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootConceptsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootConceptsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bassit/springbootconcepts/SpringbootConceptsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bassit.springbootconcepts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootConceptsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
