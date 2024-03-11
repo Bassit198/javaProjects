10b372721c9e6108bfe08916964a0a81dcaadff5
Bassit <bassitilahi19@gmail.com>
2024-03-11 11:40:37 -0400

    Added encryptor method

--- src/main/java/org/example/passwordmanager/Services/Encryptor.java
+++ src/main/java/org/example/passwordmanager/Services/Encryptor.java
@@ -1,4 +1,36 @@
 package org.example.passwordmanager.Services;
 
 public class Encryptor {
+
+    public static String encrypt(String text) {
+        int key = 6;
+        char[] chars = text.toCharArray();
+        for(int i=0; i<chars.length; i++) {
+            if(i%2==0) {
+                chars[i]+=key;
+            }else {
+                chars[i]-=key;
+            }
+
+        }
+        String encrypt = String.copyValueOf(chars);
+        return encrypt;
+
+    }
+
+    public static String decrypt(String text) {
+        int key = 6;
+        char[] chars = text.toCharArray();
+        for(int i=0; i<chars.length; i++) {
+            if(i%2==0) {
+                chars[i]-=key;
+            }else {
+                chars[i]+=key;
+            }
+
+        }
+        String decrypt = String.copyValueOf(chars);
+        return decrypt;
+
+    }
 }
