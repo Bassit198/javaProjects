6ea8b9a37bc7b91271e057d08d7db65b21ace11c
Bassit <bassitilahi19@gmail.com>
2024-03-11 11:27:12 -0400

    Added DB connection and tables

--- src/main/java/org/example/passwordmanager/Models/Users.java
+++ src/main/java/org/example/passwordmanager/Models/Users.java
@@ -1,4 +1,25 @@
 package org.example.passwordmanager.Models;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
+    private long id;
+
+    @Column
+    private String firstname, lastname, email;
+
+    @Column
+    @JsonIgnore
+    private String username, password;
 }
