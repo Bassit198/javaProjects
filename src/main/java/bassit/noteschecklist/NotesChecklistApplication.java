74caa525059e9a9d273607c96b21e64c805ae392
Bassit <bassitilahi19@gmail.com>
2024-05-07 11:46:43 -0400

    initial commit

---
+++ src/main/java/bassit/noteschecklist/NotesChecklistApplication.java
@@ -0,0 +1,13 @@
+package bassit.noteschecklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesChecklistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotesChecklistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bassit/noteschecklist/NotesChecklistApplicationTests.java
@@ -0,0 +1,13 @@
+package bassit.noteschecklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesChecklistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
